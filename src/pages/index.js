3fcdf4a26628c2e6b5566369b5422c6f9f8291af
Karl Melecio <karlmelecio04@gmail.com>
2019-10-15 18:54:40 +0800

    initial push

---
+++ src/pages/index.js
@@ -0,0 +1,65 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+
+import Bio from "../components/bio"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import { rhythm } from "../utils/typography"
+
+class BlogIndex extends React.Component {
+  render() {
+    const { data } = this.props
+    const siteTitle = data.site.siteMetadata.title
+    const posts = data.allContentfulPost.edges
+
+    return (
+      <Layout location={this.props.location} title={siteTitle}>
+        <SEO title="All posts" />
+        <Bio />
+        {posts.map(({ node }) => {
+          const title = node.title || node.slug
+          return (
+            <article key={node.slug}>
+              <header>
+                <h3
+                  style={{
+                    marginBottom: rhythm(1 / 4),
+                  }}
+                >
+                  <Link style={{ boxShadow: `none` }} to={node.slug}>
+                    {title}
+                  </Link>
+                </h3>
+              </header>
+              <section> 
+                <p>{node.subtitle}</p>
+              </section>
+            </article>
+          )
+        })}
+      </Layout>
+    )
+  }
+}
+
+export default BlogIndex
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allContentfulPost {
+      edges {
+        node {
+          title
+          subtitle
+          author
+          slug
+        }
+      }
+    }
+  }
+`
---
+++ src/templates/blog-post-contentful.js
@@ -0,0 +1,95 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+
+import Bio from "../components/bio"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import { rhythm } from "../utils/typography"
+
+class BlogPostContentfulTemplate extends React.Component {
+  render() {
+    const post = this.props.data.contentfulPost
+    const siteTitle = this.props.data.site.siteMetadata.title
+    const { previous, next } = this.props.pageContext
+
+    return (
+      <Layout location={this.props.location} title={siteTitle}>
+        <SEO
+          title={post.title}
+          description={post.subtitle}
+
+        />
+        <article>
+          <header>
+            <h1
+              style={{
+                marginTop: rhythm(1),
+                marginBottom: 0,
+              }}
+            >
+              {post.title}
+            </h1>
+          </header>
+          <section dangerouslySetInnerHTML={{ __html: post.content.childContentfulRichText.html }} />
+          <hr
+            style={{
+              marginBottom: rhythm(1),
+            }}
+          />
+          <footer>
+            <Bio />
+          </footer>
+        </article>
+
+        <nav>
+          <ul
+            style={{
+              display: `flex`,
+              flexWrap: `wrap`,
+              justifyContent: `space-between`,
+              listStyle: `none`,
+              padding: 0,
+            }}
+          >
+            <li>
+              {previous && (
+                <Link to={previous.slug} rel="prev">
+                  ← {previous.title}
+                </Link>
+              )}
+            </li>
+            <li>
+              {next && (
+                <Link to={next.slug} rel="next">
+                  {next.title} →
+                </Link>
+              )}
+            </li>
+          </ul>
+        </nav>
+      </Layout>
+    )
+  }
+}
+
+export default BlogPostContentfulTemplate
+
+export const pageQuery = graphql`
+  query ContentfulBlogPostBySlug($slug: String!) {
+    site {
+      siteMetadata {
+        title
+        author
+      }
+    }
+    contentfulPost( slug: { eq: $slug}){
+      title
+      author
+      content{
+        childContentfulRichText{
+          html
+        }
+      }
+    }
+  }
+`
